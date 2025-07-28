bba52f27117bf72c3432fb99db4f3ee3353f0036
Marek Hanzal <marek.hanzal@icloud.com>
2025-07-29 01:11:55 +0200

    Refactor locale route to improve loading process by separating beforeLoad and loader functions, enhancing code clarity and maintainability.

--- src/@routes/$locale.tsx
+++ src/@routes/$locale.tsx
@@ -3,8 +3,10 @@ import { translator } from "@use-pico/common";
 import { bootstrap } from "~/app/database/kysely";
 
 export const Route = createFileRoute("/$locale")({
-	async loader({ params: { locale } }) {
+	async beforeLoad() {
 		await bootstrap();
+	},
+	async loader({ params: { locale } }) {
 		try {
 			translator.push(
 				(await import(`../translation/${locale}.yaml`)).default,
