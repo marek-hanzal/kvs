701f2d9dbc65879402e16fc1bc88d777aff4621a
Marek Hanzal <marek.hanzal@icloud.com>
2025-07-28 23:58:01 +0200

    Refactor Kysely integration to streamline database setup and bootstrap process, and update locale route to include database initialization.

--- src/@routes/$locale.tsx
+++ src/@routes/$locale.tsx
@@ -1,8 +1,10 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { translator } from "@use-pico/common";
+import { bootstrap } from "~/app/database/kysely";
 
 export const Route = createFileRoute("/$locale")({
 	async loader({ params: { locale } }) {
+		await bootstrap();
 		try {
 			translator.push(
 				(await import(`../translation/${locale}.yaml`)).default,
--- src/app/database/kysely.ts
+++ src/app/database/kysely.ts
@@ -1,54 +1,9 @@
-import type { Database } from "@use-pico/common";
-import { Kysely } from "kysely";
-// @ts-ignore
-import { SQLocalKysely } from "sqlocal/kysely";
+import { withDatabase } from "@use-pico/client";
+import type { Database } from "~/app/database/Database";
 
-export namespace withDatabase {
-	export namespace Props {
-		export namespace bootstrap {
-			export interface Props<TDatabase> {
-				kysely: Kysely<TDatabase>;
-			}
-
-			export type Callback<TDatabase> = (
-				props: Props<TDatabase>,
-			) => Promise<any>;
-		}
-	}
-
-	export interface Props<TDatabase> {
-		database: string;
-		bootstrap: Props.bootstrap.Callback<TDatabase>;
-	}
-}
-
-export const withDatabase = <TDatabase>({
-	database,
-	bootstrap,
-}: withDatabase.Props<TDatabase>): Database.Instance<TDatabase> => {
-	const { dialect } = new SQLocalKysely({
-		databasePath: `${database}.sqlite3`,
-		onInit(sql: any) {
-			return [
-				sql`PRAGMA foreign_keys = ON;`,
-				sql`PRAGMA journal_mode = WAL;`,
-			];
-		},
-	});
-
-	const kysely = new Kysely<TDatabase>({
-		dialect,
-		log: [
-			"error",
-		],
-	});
-
-	return {
-		kysely,
-		async bootstrap() {
-			return bootstrap({
-				kysely,
-			});
-		},
-	};
-};
+export const { kysely, bootstrap } = withDatabase<Database>({
+	database: "kvs",
+	async bootstrap() {
+		//
+	},
+});
